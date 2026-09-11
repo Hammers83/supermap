dc87a8e366053daeba71a005106ef772821baaa5
Hammers83 <amfitness83@gmail.com>
2026-09-11 12:27:14 +0200

    Create main SuperMap component

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main>
+      <h1>SuperMap</h1>
+
+      <p>
+        Trova rapidamente i prodotti all'interno del supermercato.
+      </p>
+
+      <input
+        type="search"
+        placeholder="Cerca un prodotto..."
+        aria-label="Cerca un prodotto"
+      />
+    </main>
+  );
+}
+
+export default App;
