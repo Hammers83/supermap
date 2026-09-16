dee4da11664a2d832f9fb7909bf0d5e404e694ec
Hammers83 <amfitness83@gmail.com>
2026-09-16 12:10:20 +0200

    Add 'id' property to ProductVariant and pastaVariants

--- src/products.ts
+++ src/products.ts
@@ -1,4 +1,5 @@
 export type ProductVariant = {
+  id: string;
   name: string;
   aisle: string;
   department: string;
@@ -7,24 +8,28 @@ export type ProductVariant = {
 
 export const pastaVariants: ProductVariant[] = [
   {
+    id: "pasta-normale",
     name: "Pasta normale",
     aisle: "Corsia 7",
     department: "Pasta e prodotti secchi",
     landmark: "Riso e legumi",
   },
   {
+    id: "pasta-senza-glutine",
     name: "Pasta senza glutine",
     aisle: "Corsia 3",
     department: "Prodotti senza glutine",
     landmark: "Prodotti biologici",
   },
   {
+    id: "pasta-fresca",
     name: "Pasta fresca",
     aisle: "Banco frigo",
     department: "Banco frigo",
     landmark: "Latticini",
   },
   {
+    id: "pasta-all-uovo",
     name: "Pasta all'uovo",
     aisle: "Corsia 8",
     department: "Pasta e prodotti secchi",
